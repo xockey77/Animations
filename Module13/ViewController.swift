b44e7e377550c146bd2d6d9b32c0bf9c6bbd78f0
Andrey Belov <belov_andrey@hotmail.com>
2022-01-26 16:32:14 +0500

    Initial Commit

---
+++ Module13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Module13
+//
+//  Created by username on 26.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
